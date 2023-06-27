29478f012d916124b133934da1e70a04cdad643a
hablajl <136637926+hablajl@users.noreply.github.com>
2023-06-28 02:10:38 +0700

    Merge pull request

---
+++ tree_mountain_lion.js
@@ -0,0 +1,468 @@
+//1
+var successTools = {};
+
+//2
+successTools.goalSetting = {};
+
+//3
+successTools.goalSetting.startSmall = function(){
+	console.log("Start small to prove yourself and increase your self-confidence.");
+};
+
+//4
+successTools.goalSetting.makeActionSteps = function(){
+	console.log("Break down large goals into small achievable action steps.");
+};
+
+//5
+successTools.goalSetting.trackProgress = function(){
+	console.log("Keep track of your progress to stay motivated and on track.");
+};
+
+//6
+successTools.timeManagement = {};
+
+//7
+successTools.timeManagement.planSchedule = function(){
+	console.log("Create a daily and weekly schedule to help you plan your time.");
+};
+
+//8
+successTools.timeManagement.rankPriorities = function(){
+	console.log("Rank priorities to ensure that you are addressing important tasks first.");
+};
+
+//9
+successTools.timeManagement.scheduleBreaks = function(){
+	console.log("Schedule regular breaks to give yourself time to recharge and remain focused on tasks.");
+};
+
+//10
+successTools.selfCare = {};
+
+//11
+successTools.selfCare.practiceMindfulness = function(){
+	console.log("Practice mindfulness regularly to increase focus and reduce stress.");
+};
+
+//12
+successTools.selfCare.getEnoughSleep = function(){
+	console.log("Get enough sleep to ensure that you have the energy and focus to tackle your tasks.");
+};
+
+//13
+successTools.selfCare.eatHealthyFood = function(){
+	console.log("Eat healthy food to give your body the fuel it needs to stay energized and focused.");
+};
+
+//14
+successTools.stressManagement = {};
+
+//15
+successTools.stressManagement.recognizeSigns = function(){
+	console.log("Recognize the signs of stress so you can take steps to address it.");
+};
+
+//16
+successTools.stressManagement.talkToSomeone = function(){
+	console.log("Talk to someone when stress gets overwhelming to help you handle it.");
+};
+
+//17
+successTools.stressManagement.practiceRelaxation = function(){
+	console.log("Practice relaxation techniques to help you calm down and reset.");
+};
+
+//18
+successTools.selfAwareness = {};
+
+//19
+successTools.selfAwareness.knowYourLimits = function(){
+	console.log("Know your limits to ensure that you aren't taking on more than you can manage.");
+};
+
+//20
+successTools.selfAwareness.prioritizeGoals = function(){
+	console.log("Prioritize goals to make sure that you are focusing on the right things.");
+};
+
+//21
+successTools.selfAwareness.knowStrengths = function(){
+	console.log("Know your strengths and weaknesses so that you can capitalize on the former and work on the latter.");
+};
+
+//22
+successTools.motivation = {};
+
+//23
+successTools.motivation.setPositiveGoals = function(){
+	console.log("Set positive goals that you are motivated to achieve.");
+};
+
+//24
+successTools.motivation.stayFocused = function(){
+	console.log("Stay focused and motivated for the long-term by breaking down goals into achievable steps.");
+};
+
+//25
+successTools.motivation.rewardYourself = function(){
+	console.log("Reward yourself for reaching milestones by celebrating your achievements.");
+};
+
+//26
+successTools.problemSolving = {};
+
+//27
+successTools.problemSolving.defineProblem = function(){
+	console.log("Define the problem so you can start looking for solutions.");
+};
+
+//28
+successTools.problemSolving.brainstormSolutions = function(){
+	console.log("Brainstorm potential solutions and consider the pros and cons of each one.");
+};
+
+//29
+successTools.problemSolving.implementSolution = function(){
+	console.log("Implement the best solution, and track your progress to ensure that it is addressing the issue.");
+};
+
+//30
+successTools.networking = {};
+
+//31
+successTools.networking.identifyConnections = function(){
+	console.log("Identify potential connections who can help you achieve your goals.");
+};
+
+//32
+successTools.networking.buildRelationships = function(){
+	console.log("Build relationships with those connections and create a network of support.");
+};
+
+//33
+successTools.networking.maintainConnections = function(){
+	console.log("Maintain those connections by staying in touch and offering support when appropriate.");
+};
+
+//34
+successTools.resilience = {};
+
+//35
+successTools.resilience.practiceSelfCompassion = function(){
+	console.log("Practice self-compassion and forgive yourself for mistakes.");
+};
+
+//36
+successTools.resilience.acceptChange = function(){
+	console.log("Accept that change is a part of life and learn to adapt to it.");
+};
+
+//37
+successTools.resilience.keepGoing = function(){
+	console.log("Keep going even when things get tough and you're tempted to give up.");
+};
+
+//38
+successTools.creativity = {};
+
+//39
+successTools.creativity.trySomethingNew = function(){
+	console.log("Try something new to stretch your creativity and approach problems from a different perspective.");
+};
+
+//40
+successTools.creativity.exploreIdeas = function(){
+	console.log("Explore ideas and push past the boundaries of conventional thinking to find creative solutions.");
+};
+
+//41
+successTools.creativity.reframeProblems = function(){
+	console.log("Reframe problems to look at them from different angles and come up with innovative solutions.");
+};
+
+//42
+successTools.selfDiscipline = {};
+
+//43
+successTools.selfDiscipline.developRoutines = function(){
+	console.log("Develop routines and rituals to help you stay on track and reach your goals.");
+};
+
+//44
+successTools.selfDiscipline.avoidProcrastination = function(){
+	console.log("Avoid procrastination by taking action and making decisions when necessary.");
+};
+
+//45
+successTools.selfDiscipline.manageDistractions = function(){
+	console.log("Manage distractions and stay focused on tasks at hand.");
+};
+
+//46
+successTools.flexibility = {};
+
+//47
+successTools.flexibility.embraceFailure = function(){
+	console.log("Embrace failure as a learning experience and an opportunity to grow.");
+};
+
+//48
+successTools.flexibility.adjustGoals = function(){
+	console.log("Adjust goals as needed and be open to changing course as needed.");
+};
+
+//49
+successTools.flexibility.remainOpenMinded = function(){
+	console.log("Remain open-minded and flexible in the face of change and unexpected obstacles.");
+};
+
+//50
+successTools.persistence = {};
+
+//51
+successTools.persistence.overcomeFear = function(){
+	console.log("Overcome fear and take risks to take your skills and success to the next level.");
+};
+
+//52
+successTools.persistence.persevere = function(){
+	console.log(" Persevere even when faced with challenges or obstacles.");
+};
+
+//53
+successTools.persistence.takeCalculatedRisks = function(){
+	console.log("Take calculated risks and use failures as learning experiences to improve.");
+};
+
+//54
+successTools.positiveThinking = {};
+
+//55
+successTools.positiveThinking.identifyNegativeThoughts = function(){
+	console.log("Identify negative thoughts and reframe them into positive affirmations.");
+};
+
+//56
+successTools.positiveThinking.shiftPerspective = function(){
+	console.log("Shift perspective to see the glass half full instead of half empty.");
+};
+
+//57
+successTools.positiveThinking.learnToAccept = function(){
+	console.log("Learn to accept yourself and others and focus on the good instead of the bad.");
+};
+
+//58
+successTools.communication = {};
+
+//59
+successTools.communication.listen = function(){
+	console.log("Listen to understand instead of just responding.");
+};
+
+//60
+successTools.communication.askQuestions = function(){
+	console.log("Ask questions to gain clarity and better understand the situation.");
+};
+
+//61
+successTools.communication.expressYourself = function(){
+	console.log("Express yourself clearly and concisely to make sure that your message is understood.");
+};
+
+//62
+successTools.emotionalIntelligence = {};
+
+//63
+successTools.emotionalIntelligence.recognizeEmotions = function(){
+	console.log("Recognize and understand your emotions and the emotions of others.");
+};
+
+//64
+successTools.emotionalIntelligence.manageEmotions = function(){
+	console.log("Manage your emotions and remain calm and composed in stressful situations.");
+};
+
+//65
+successTools.emotionalIntelligence.communicateEffectively = function(){
+	console.log("Communicate effectively even when emotions are high to ensure that all parties are heard and respected.");
+};
+
+//66
+successTools.collaboration = {};
+
+//67
+successTools.collaboration.workTogether = function(){
+	console.log("Work together as a team to reach shared goals.");
+};
+
+//68
+successTools.collaboration.valueOthers = function(){
+	console.log("Value the contributions of others and recognize their strengths and achievements.");
+};
+
+//69
+successTools.collaboration.remainOpenToIdeas = function(){
+	console.log("Remain open to new ideas and perspectives and use them to create innovative solutions.");
+};
+
+//70
+successTools.leadership = {};
+
+//71
+successTools.leadership.setExample = function(){
+	console.log("Set a good example and lead by example to motivate and inspire others.");
+};
+
+//72
+successTools.leadership.provideFeedback = function(){
+	console.log("Provide constructive feedback to help others grow and develop their skills.");
+};
+
+//73
+successTools.leadership.facilitateDialogue = function(){
+	console.log("Facilitate dialogue to foster collaboration and gather feedback from team members.");
+};
+
+//74
+successTools.teamBuilding = {};
+
+//75
+successTools.teamBuilding.buildTrust = function(){
+	console.log("Build trust between team members to ensure that everyone is working together effectively.");
+};
+
+//76
+successTools.teamBuilding.encourageCollaboration = function(){
+	console.log("Encourage collaboration to ensure that everyone's ideas and contributions are valued.");
+};
+
+//77
+successTools.teamBuilding.developTeamSkills = function(){
+	console.log("Develop team skills to help the team work effectively together.");
+};
+
+//78
+successTools.riskTaking = {};
+
+//79
+successTools.riskTaking.pushBoundaries = function(){
+	console.log("Push boundaries and take risks to increase your potential for success.");
+};
+
+//80
+successTools.riskTaking.seekOpportunities = function(){
+	console.log("Seek out opportunities to take risks and learn from them.");
+};
+
+//81
+successTools.riskTaking.prepareForFailure = function(){
+	console.log("Prepare for failure but remain determined and focused on your goals.");
+};
+
+//82
+successTools.adaptability = {};
+
+//83
+successTools.adaptability.changeMindset = function(){
+	console.log("Change your mindset to remain flexible and open to new ideas and perspectives.");
+};
+
+//84
+successTools.adaptability.respondToChange = function(){
+	console.log("Respond to change quickly and adapt to new situations or environments.");
+};
+
+//85
+successTools.adaptability.acknowledgeMistakes = function(){
+	console.log("Acknowledge mistakes and use them as opportunities for growth and improvement.");
+};
+
+//86
+successTools.organization = {};
+
+//87
+successTools.organization.createSystems = function(){
+	console.log("Create systems and processes to stay organized and streamline tasks.");
+};
+
+//88
+successTools.organization.prioritizeTasks = function(){
+	console.log("Prioritize tasks and break them down into manageable steps to stay focused and on track.");
+};
+
+//89
+successTools.organization.useTimeEfficiently = function(){
+	console.log("Use time efficiently by setting deadlines for tasks and focusing on one task at a time.");
+};
+
+//90
+successTools.research = {};
+
+//91
+successTools.research.askQuestions = function(){
+	console.log("Ask questions to gain an understanding of the topic and make sure you have accurate information.");
+};
+
+//92
+successTools.research.gatherData = function(){
+	console.log("Gather data from multiple sources, and analyze it to draw conclusions and make informed decisions.");
+};
+
+//93
+successTools.research.investigateFacts = function(){
+	console.log("Investigate facts to ensure that the information you have is accurate and reliable.");
+};
+
+//94
+successTools.decisionMaking = {};
+
+//95
+successTools.decisionMaking.weighOptions = function(){
+	console.log("Weigh options and consider the pros and cons of each one to make the best decision.");
+};
+
+//96
+successTools.decisionMaking.acceptResponsibility = function(){
+	console.log("Accept responsibility for your decisions and take ownership of their outcomes.");
+};
+
+//97
+successTools.decisionMaking.remainFlexible = function(){
+	console.log("Remain flexible and open to changing your decisions if needed.");
+};
+
+//98
+successTools.criticalThinking = {};
+
+//99
+successTools.criticalThinking.analyzeSituation = function(){
+	console.log("Analyze the situation to understand it better and identify potential solutions.");
+};
+
+//100
+successTools.criticalThinking.evaluateEvidence = function(){
+	console.log("Evaluate evidence and consider the facts before forming an opinion or making a decision.");
+};
+
+//101
+successTools.criticalThinking.askQuestions = function(){
+	console.log("Ask questions to gain a deeper understanding and clarify any misunderstandings.");
+};
+
+//102
+successTools.collaboration = {};
+
+//103
+successTools.collaboration.workTogether = function(){
+	console.log("Work together as a team to reach shared goals.");
+};
+
+//104
+successTools.collaboration.valueOthers = function(){
+	console.log("Value the contributions of others and recognize their strengths and achievements.");
+};
+
+//105
+successTools.
